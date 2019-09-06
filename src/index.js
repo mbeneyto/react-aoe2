70f62e6a69be2e668ca92718afc72654d5f1844d
Marcos Beneyto <mbeneyto93@gmail.com>
2019-09-06 14:18:00 +0200

    Added polyfills for IE11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
-import App from "./components/App";
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
